6c6ba25f4c09d1819efc6b8775a71b417e82d3ad
Zhengqbbb <1074059947@qq.com>
2021-12-03 09:32:59 +0800

    feat(install): make install install shell script easy

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 # shellcheck shell=sh disable=SC3043
 
-____font_test_run() {
-    local _source_path
-    _source_path="$( cd "$( dirname "$0" )" && pwd )"
+____font_install_run() {
+    if [ -z "$(command -v git)" ];then
+        printf "\033[0;31m[qb]: Error git is not installed\033[0m\n"
+        return 1
+    fi
     local font_dir
+    local _source_path
+    local REMOTE="${REMOTE:-github}"
+    local GUSER="${REMOTE:-zhengqbbb}"
+    _source_path="$( cd "$( dirname "$0" )" && pwd )"
+    env git clone --depth=1 "https://${REMOTE}.com/${GUSER}/MonacoForPowerline.git" "$_source_path" || {
+        printf "\033[0;31m[qb]: Error git clone of MonacoForPowerline repo failed[0m\n"
+        return 1
+    }
     if test "$(uname)" = "Darwin" ; then
         # MacOS
         font_dir="$HOME/Library/Fonts"
@@ -12,11 +22,11 @@ ____font_test_run() {
         font_dir="$HOME/.local/share/fonts"
         mkdir -p "$font_dir"
     fi
-    cp "$_source_path/MonacoForPowerline.ttf" "$font_dir/"
+    cp "$_source_path/MonacoForPowerline/MonacoForPowerline.ttf" "$font_dir/"
     if which fc-cache >/dev/null 2>&1 ; then
-        echo "Resetting font cache, this may take a moment..."
+        printf "\033[0;33m[qb]: Resetting font cache, this may take a moment...\033[0m\n"
         fc-cache -f "$font_dir"
     fi
-    echo "Powerline fonts installed to $font_dir"
+    printf "\033[1;32m[qb]: Powerline fonts installed to \033[4;33m$font_dir\033[0m\n"
 }
-____font_test_run
+____font_install_run
